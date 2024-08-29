cfe1b515908eabd93c417421c9a50f2b6ca41481
KUNAL Singh <68858909+kunal-singh-8057@users.noreply.github.com>
2024-08-28 21:21:15 -0700

    Update Viewbooks.js

--- client/src/Viewbooks.js
+++ client/src/Viewbooks.js
@@ -9,7 +9,7 @@ const Viewbooks = () => {
 
 
   useEffect(()=>{
-    Axios.get("https://mern-book-app-three.vercel.app/getbooks")
+    Axios.get("https://mern-book-app-phi.vercel.app/getbooks")
 
     .then((response)=>{
       setusers(response.data);
